1c21e62251c26db879894c4166632c13859f558f
PrisaMG <49337422+PrisaMG@users.noreply.github.com>
2019-04-06 13:01:59 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,36 @@
 import discord
+import asyncio
+import youtube_dl
+import os
+from discord.ext import commands
+from discord.ext.commands import Bot
+
+
+bot=commands.Bot(command_prefix='!')
+bot.remove_command('help')
+
+extensions = [ 'help']
+
+async def my_background_task():
+    await bot.wait_until_ready()
+    while not bot.is_closed:
+        await bot.change_presence(game=discord.Game(name='test3')) 
+        await asyncio.sleep(10)
+        await bot.change_presence(game=discord.Game(name='test2'))
+        await asyncio.sleep(10)
+        await bot.change_presence(game=discord.Game(name="test1"))
+        await asyncio.sleep(10)
+        
+@bot.event
+async def on_ready():
+    print("hi")
+    
+if __name__ == '__main__':
+  for extension in extensions:
+    try:
+      bot.load_extension(extension)
+    except Exception as error:
+      print('{} cannot be loaded. [{}]'.format(extension, error))
+ 
+bot.loop.create_task(my_background_task())
+bot.run(os.environ['BOT_TOKEN'])
