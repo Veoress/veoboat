413986644ba5c5d058c0e29f14ac64eddab06696
PrisaMG <49337422+PrisaMG@users.noreply.github.com>
2019-04-06 22:30:06 +0200

    Update join.py

--- data/join.py
+++ data/join.py
@@ -1,1 +1,13 @@
+import discord
+from discord.ext import commands
 
+class Join:
+  def __init__(self, bot):
+    self.bot = bot
+    
+  @commands.command()
+  async def test(self):
+
+    
+def setup(bot):
+  bot.add_cog(Join(bot))
