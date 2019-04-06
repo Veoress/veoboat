432805d5196b581e01a2cf8ac89b9f739c890496
PrisaMG <49337422+PrisaMG@users.noreply.github.com>
2019-04-06 14:45:11 +0200

    Update help.py

--- data/help.py
+++ data/help.py
@@ -7,9 +7,9 @@ class Helper:
     
   @commands.command()
   async def help(self):
-    await self.bot.say('4 |General: ping, ahoj, help, userinfo')
-    await self.bot.say('3 |Fun: number, rdance, fortmap')
-    await self.bot.say('2 |Bot: invite, veo_server')
+    await self.bot.say('4 |General: ```ping, ahoj, help, userinfo```')
+    await self.bot.say('3 |Fun: ```number, rdance, fortmap```')
+    await self.bot.say('2 |Bot: ```invite, veo_server```')
     return
 
     
