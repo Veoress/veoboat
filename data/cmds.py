235c811c83c8fd446342760cd962f9365aadbc6b
PrisaMG <49337422+PrisaMG@users.noreply.github.com>
2019-04-06 15:31:19 +0200

    Update cmds.py

--- data/cmds.py
+++ data/cmds.py
@@ -1,12 +1,12 @@
 import discord
 from discord.ext import commands
 
-class Helper2:
+class Cmds:
   def __init__(self, bot):
     self.bot = bot
     
   @commands.command()
-  async def testhelp(self):
+  async def load(self):
     await self.bot.say('**test Help 4 |General:**   `ping, ahoj, help, userinfo `')
     await self.bot.say('**test Help 3 |Fun:**  `number, rdance, fortmap `')
     await self.bot.say('**test Help 2 |Bot:**  `invite, veo_server `')
@@ -14,4 +14,4 @@ class Helper2:
 
     
 def setup(bot):
-  bot.add_cog(Helper2(bot))
+  bot.add_cog(Cmds(bot))
