2c0b9d66dcd6bfa861f9a9279da320154cb421a6
PrisaMG <49337422+PrisaMG@users.noreply.github.com>
2019-04-06 16:33:23 +0200

    Update userinfo.py

--- data/userinfo.py
+++ data/userinfo.py
@@ -6,9 +6,23 @@ class Userinfo:
     self.bot = bot
     
   @commands.command()
-  async def userinfo(self):
+  async def setuserinfo(self):
     await self.bot.say('userinfo command je v modu nastavování')
     return
+  
+  @command.command(pass_context=True)
+  async def userinfo(ctx, user: discord.Member=None):
+    if not user:
+      user = ctx.message.author
+
+    embed=discord.Embed(title="JMÉNO", description=" {} ".format(user.mention), color=0xfed83d)
+    embed.set_author(name=" ")
+    embed.set_image(url=user.avatar_url)
+    embed.add_field(name="**STATUS**", value=" {} ".format(user.status), inline=True)
+    embed.add_field(name="**NEJVĚTŠÍ ROLE**", value=" {} ".format(user.top_role.mention), inline=True)
+    embed.add_field(name="**PŘIPOJIL SE**", value=" {} ".format(user.joined_at.strftime(" %d %n %Y  %k:%M:%S ")), inline=False)
+    embed.add_field(name="**ID**", value=" {} ".format(user.id), inline=False)
+    await self.bot.send_message(ctx.message.channel, embed=embed)
 
     
 def setup(bot):
