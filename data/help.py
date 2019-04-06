784147a283a127239236a99028d57d708ef98d88
PrisaMG <49337422+PrisaMG@users.noreply.github.com>
2019-04-06 22:49:01 +0200

    Update help.py

--- data/help.py
+++ data/help.py
@@ -9,9 +9,9 @@ class Helper:
   async def help(self):
     embed=discord.Embed(title="**Help**", description="** **", color=0x54ce1a)
     embed.set_author(name=" ")
-    embed.add_field(name="**4 |General:**", value="`ping, ahoj, help, userinfo `", inline=True)
-    embed.add_field(name="**3 |Fun:**", value="`number, rdance, fortmap `", inline=True)
-    embed.add_field(name="**2 |Bot:**", value="`number, rdance, fortmap `", inline=True)
+    embed.add_field(name="**4 |General:**", value="`ping, ahoj, help, userinfo `", inline=False)
+    embed.add_field(name="**3 |Fun:**", value="`number, rdance, fortmap `", inline=False)
+    embed.add_field(name="**2 |Bot:**", value="`invite, veo_server `", inline=False)
     await self.bot.say(embed=embed)
     return
 
