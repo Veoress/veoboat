b9209b661b613e8452c47ec5c258c96024d21288
PrisaMG <49337422+PrisaMG@users.noreply.github.com>
2019-04-06 22:44:50 +0200

    Update help.py

--- data/help.py
+++ data/help.py
@@ -6,14 +6,13 @@ class Helper:
     self.bot = bot
     
   @commands.command()
-  async def help(self, ctx):
-    
+  async def help(self):
     embed=discord.Embed(title="**Help**", description="** **", color=0x54ce1a)
     embed.set_author(name=" ")
     embed.add_field(name="**4 |General:**", value="`ping, ahoj, help, userinfo `", inline=True)
     embed.add_field(name="**3 |Fun:**", value="`number, rdance, fortmap `", inline=True)
     embed.add_field(name="**2 |Bot:**", value="`number, rdance, fortmap `", inline=True)
-    await self.bot.send_message(ctx.message.channel, embed=embed)
+    await self.bot.say(embed=embed)
     return
 
     
