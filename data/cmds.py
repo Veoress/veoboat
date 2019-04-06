252705572a6940765261bfcdba5ac39bc29fa9b1
PrisaMG <49337422+PrisaMG@users.noreply.github.com>
2019-04-06 15:22:18 +0200

    Update cmds.py

--- data/cmds.py
+++ data/cmds.py
@@ -1,15 +1,15 @@
 import discord
 from discord.ext import commands
 
-class Basic:
+class Commaner:
   def __init__(self, bot):
     self.bot = bot
     
-  @command.command()
+  @commands.command()
   async def load(self):
-    await self.bot.say('uložiště **COMMANDS** bylo načteno')
+    await self.bot.say('uložiště: **COMMANDS** bylo načteno')
     return
 
     
 def setup(bot):
-  bot.add_cog(Basic(bot))
+  bot.add_cog(Commander(bot))
