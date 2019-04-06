5b82cea45a80af14ae683270c48ab04277d62551
PrisaMG <49337422+PrisaMG@users.noreply.github.com>
2019-04-06 22:40:55 +0200

    Update join.py

--- data/join.py
+++ data/join.py
@@ -5,12 +5,8 @@ class Join:
   def __init__(self, bot):
     self.bot = bot
     
-  @commands.command()
-  async def test(self):
   async def on_member_join(self, member):
-    await bot.say()
-
-await bot.send_message(member, "Ahoj vítám tě na serveru GamingNetwork. přečti si prosím pravidla a faq") 
+    await bot.send_message(member, "Ahoj vítám tě na serveru GamingNetwork. přečti si prosím pravidla a faq") 
     
 def setup(bot):
   bot.add_cog(Join(bot))
