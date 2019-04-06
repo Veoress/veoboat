149aba85fe0bfaeb57a53b6c5d1bc0e760815e4a
PrisaMG <49337422+PrisaMG@users.noreply.github.com>
2019-04-06 21:32:54 +0200

    Update userinfo.py

--- data/userinfo.py
+++ data/userinfo.py
@@ -10,8 +10,16 @@ class Userinfo:
     if not user:
       user = ctx.message.author
       
+    embed=discord.Embed(title="**JMÉNO**", description="<@{}>".format(user.id), color=0xfed83d)
+    embed.set_author(name=" ")
+    embed.set_image(url=user.avatar_url)
+    embed.add_field(name="**STATUS**", value=" {} ".format(user.status), inline=True)
+    embed.add_field(name="**NEJVĚTŠÍ ROLE**", value=" {} ".format(user.top_role.mention), inline=True)
+    embed.add_field(name="**PŘIPOJIL SE**", value=" {} ".format(user.joined_at.strftime(" %d %n %Y  %k:%M:%S ")), inline=False)
+    embed.add_field(name="**ID**", value=" {} ".format(user.id), inline=False)
+    await self.bot.send_message(ctx.message.channel, embed=embed)
+    
     await self.bot.say('userinfo command je v modu nastavování')
-    await self.bot.say('user <@{}>'.format(user.id))
     return
 
     
