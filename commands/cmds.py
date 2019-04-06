82a851edd41ae26f05f6ed0b8db2d06bb431d4d5
PrisaMG <49337422+PrisaMG@users.noreply.github.com>
2019-04-06 13:26:11 +0200

    Update cmds.py

--- commands/cmds.py
+++ commands/cmds.py
@@ -1,1 +1,14 @@
+import discord
+from discord.ext import commands
 
+class Helper:
+  def __init__(self, bot):
+    self.bot = bot
+    
+  @commands.command()
+  async def test(self):
+    await self.bot.say('test')
+
+    
+def setup(bot):
+  bot.add_cog(Helper(bot))
