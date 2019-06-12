753bf6cafe7f6674ffd3f0e046c618532cb84179
Veoress <49337232+Veoress@users.noreply.github.com>
2019-06-12 12:38:50 +0200

    cmds.py

--- data/cmds.py
+++ data/cmds.py
@@ -15,21 +15,12 @@ class Cmds:
   async def ahoj(self):
     await self.bot.say('**Ahoj :D**')
     return
+
   @commands.command(pass_context=True)
   async def number(self, ctx):
     await self.bot.say(random.randint(1,100))
     return
- 
 
-
-@commands.command(pass_context=True)
-  async def rdance(self, ctx):
-    embed=discord.Embed(title=" ", description=" ", color=0xb51ea9)
-    embed.set_author(name=" ")
-    embed.set_image(url="https://images.app.goo.gl/tn9jbMAxrdqjARd4A") 
-    embed.add_field(name="**Rainbow Dance**", value="** **", inline=True)
-    await self.bot.send_message(ctx.message.channel, embed=embed)
-    return
     
 def setup(bot):
   bot.add_cog(Cmds(bot))
