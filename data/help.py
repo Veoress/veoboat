7ad531a70a9125e3d031fc66e96d17b3d82451f7
Veoress <49337232+Veoress@users.noreply.github.com>
2019-06-12 07:19:58 +0200

    help.py

--- data/help.py
+++ data/help.py
@@ -10,7 +10,7 @@ class Helper:
     embed=discord.Embed(title="**Help**", description="** **", color=0x54ce1a)
     embed.set_author(name=" ")
     embed.add_field(name="**4 |General:**", value="`ping, ahoj, help, userinfo `", inline=False)
-    embed.add_field(name="**3 |Fun:**", value="`number, rdance, fortmap `", inline=False)
+    embed.add_field(name="**2 |Fun:**", value="`number, rdance `", inline=False)
     await self.bot.say(embed=embed)
     return
 
