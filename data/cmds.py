d6a8f41360cee6a71e52801fef632623e6b9861e
Veoress <49337232+Veoress@users.noreply.github.com>
2019-06-12 07:18:02 +0200

    cmds.py

--- data/cmds.py
+++ data/cmds.py
@@ -29,6 +29,15 @@ class Cmds:
     await self.bot.send_message(ctx.message.channel, embed=embed)
     return
 
+
+@commands.command(pass_context=True)
+  async def fortmap(self, ctx):
+    embed=discord.Embed(title=" ", description=" ", color=0xb51ea9)
+    embed.set_author(name=" ")
+    embed.set_image(url="https://images.app.goo.gl/tn9jbMAxrdqjARd4A") 
+    embed.add_field(name="**Rainbow Dance**", value="** **", inline=True)
+    await self.bot.send_message(ctx.message.channel, embed=embed)
+    return
     
 def setup(bot):
   bot.add_cog(Cmds(bot))
