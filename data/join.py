a6af5bcf74e11a4aaaa904690ea4a01a9a98e9db
Veoress <49337232+Veoress@users.noreply.github.com>
2019-06-13 14:35:21 +0200

    join.py

--- data/join.py
+++ data/join.py
@@ -6,7 +6,7 @@ class Join:
     self.bot = bot
     
   async def on_member_join(self, member):
-    await self.bot.send_message(member, "Ahoj vítám tě na serveru GamingNetwork. přečti si prosím pravidla a faq") 
+    await self.bot.send_message(member, "Ahoj vítám tě na serveru PandaGaming. přečti si prosím #⌦📚rules ") 
     
 def setup(bot):
   bot.add_cog(Join(bot))
