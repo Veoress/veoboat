2ae9016a43b37ffd2690e9b896a4c98ce0a84afc
PrisaMG <49337422+PrisaMG@users.noreply.github.com>
2019-06-17 22:07:02 +0200

    Update cmds.py

--- data/cmds.py
+++ data/cmds.py
@@ -18,17 +18,17 @@ class Cmds:
 
   @commands.command(pass_context=True)
   async def number(self, ctx):
-    await self.bot.say(random.randint(1,100))
+    await self.bot.say("Práva byla odcizena xD")
     return
 
   @commands.command()
   async def ping(self):
-    await self.bot.say('Pong!')
+    await self.bot.say('nefaká')
     return
 
   @commands.command()
   async def pgserver(self):
-    await self.bot.say('PandaGaming Server: https://discord.gg/mNEhAJA') 
+    await self.bot.say('PandaGaming Server: https://discord.gg/mNEhAJA \n tento command si klidně nechte xDDD') 
     return
 
 def setup(bot):
