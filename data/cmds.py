bd7cceecf0a6685e35e79ac0cc1f0ec0e1abf68c
Veoress <49337232+Veoress@users.noreply.github.com>
2019-06-17 13:36:33 +0200

    cmds.py

--- data/cmds.py
+++ data/cmds.py
@@ -26,6 +26,14 @@ class Cmds:
     await self.bot.say('Pong!')
     return
 
-    
+
+  @commands.command()
+  async def help(self):
+    embed=discord.Embed(title="**PandaGaming Server**", description="** **", color=0x54ce1a)
+    embed.set_author(name=" ")
+    embed.add_field(name="**Invite:**", value="`https://discord.gg/mNEhAJA`", inline=False)
+    await self.bot.say(embed=embed)
+    return
+
 def setup(bot):
   bot.add_cog(Cmds(bot))
