56a989d5bc7a593f100a4bcd1439b3ec775747a5
Veoress <49337232+Veoress@users.noreply.github.com>
2019-06-17 13:29:56 +0200

    help.py

--- data/help.py
+++ data/help.py
@@ -10,7 +10,8 @@ class Helper:
     embed=discord.Embed(title="**Help**", description="** **", color=0x54ce1a)
     embed.set_author(name=" ")
     embed.add_field(name="**4 |General:**", value="`ping, ahoj, help, userinfo `", inline=False)
-    embed.add_field(name="**2 |Fun:**", value="`number, rdance `", inline=False)
+    embed.add_field(name="**2 |Fun:**", value="`number`", inline=False)
+    embed.add_field(name="**1 |Bot:**", value="`pg_server`", inline=False)
     await self.bot.say(embed=embed)
     return
 
