6a97f0a2a6d93ad10e51303d0d6667e72c6415b4
PrisaMG <49337422+PrisaMG@users.noreply.github.com>
2019-06-17 22:08:52 +0200

    Update help.py

--- data/help.py
+++ data/help.py
@@ -9,9 +9,7 @@ class Helper:
   async def help(self):
     embed=discord.Embed(title="**Help**", description="** **", color=0x54ce1a)
     embed.set_author(name=" ")
-    embed.add_field(name="**4 |General:**", value="`ping, ahoj, help, userinfo `", inline=False)
-    embed.add_field(name="**2 |Fun:**", value="`number`", inline=False)
-    embed.add_field(name="**1 |Bot:**", value="`pgserver`", inline=False)
+    embed.add_field(name="**Pomož si sám**", value="||nemáš zač xD||", inline=False)
     await self.bot.say(embed=embed)
     return
 
